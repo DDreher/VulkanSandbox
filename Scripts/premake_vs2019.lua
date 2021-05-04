e913c11735b2a15f652d9740abd2aa96d6bc6a18
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-05-04 23:06:47 +0200

    Restructure project, add premake build script

---
+++ Scripts/premake_vs2019.lua
@@ -0,0 +1,75 @@
+require "premake_modules"
+
+local ProjectName = "VulkanSandbox"
+
+function AddSourceFiles(DirectoryPath)
+    files
+    { 
+        ("../" .. DirectoryPath .. "/**.h"),
+		("../" .. DirectoryPath .. "/**.hpp"),
+        ("../" .. DirectoryPath .. "/**.cpp"),
+        ("../" .. DirectoryPath .. "/**.inl"),
+		("../" .. DirectoryPath .. "/**.c"),
+		("../" .. DirectoryPath .. "/**.cs")
+    }
+end
+
+function IncludeModule(ModuleNames)
+	for Idx, ModuleName in pairs(ModuleNames) do
+		includedirs { ("../" .. ProjectName .. "/Source/" .. ModuleName .. "/Public/") }
+	end
+end
+
+workspace (ProjectName)
+	location "../"
+	basedir "../"
+	language "C++"
+	configurations {"Debug", "Release"}
+	platforms {"x64"}
+	warnings "default"
+	characterset ("MBCS")
+	rtti "Off"
+	toolset "v142"
+	cppdialect "C++latest"
+	--flags {"FatalWarnings"}
+	
+	filter { "configurations:Debug" }
+		runtime "Debug"
+		defines { "_DEBUG" }
+		symbols "On"
+		optimize "Off"
+		debugdir "$(SolutionDir)"
+		
+	filter { "configurations:Release" }
+	 	runtime "Release"
+		defines { "_RELEASE", "NDEBUG" }
+	 	symbols "Off"
+	 	optimize "Full"
+
+project (ProjectName)
+	location ("../" .. ProjectName)
+	targetdir ("../Build/" .. ProjectName .. "$(Configuration)_$(Platform)")
+	objdir "!../Build/Intermediate/$(ProjectName)_$(Configuration)_$(Platform)"
+	kind "ConsoleApp"
+
+	AddSourceFiles(ProjectName)
+	includedirs { "$(ProjectDir)" }
+	IncludeModule {"Core"}
+	
+	pchheader ("Core.h")
+	pchsource ("../" .. ProjectName .. "/Source/Core/Private/Core.cpp")
+	forceincludes  { "Core.h" }
+
+	disablewarnings 
+	{
+        "4100", -- unreferenced formal paramter
+		"4189"  -- local variable initalized but not referenced
+	}
+
+	AddGLFW()
+	AddVulkan()
+	AddGLM()
+
+	filter "files:**/ThirdParty/**.*"
+		flags "NoPCH"
+		disablewarnings { "4100" }
