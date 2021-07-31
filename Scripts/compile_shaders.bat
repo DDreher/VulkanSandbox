56c2365a30f6b08f98c1e5db9d53e3e15b487970
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-07-31 14:30:43 +0200

    Set correct output path of the shaders

--- Scripts/compile_shaders.bat
+++ Scripts/compile_shaders.bat
@@ -1,4 +1,4 @@
-%VULKAN_SDK%\Bin32\glslc.exe %~dp0..\assets\shaders\shader.vert -o vert.spv
-%VULKAN_SDK%\Bin32\glslc.exe %~dp0..\assets\shaders\shader.frag -o frag.spv
+%VULKAN_SDK%\Bin32\glslc.exe %~dp0..\assets\shaders\shader.vert -o %~dp0..\assets\shaders\vert.spv
+%VULKAN_SDK%\Bin32\glslc.exe %~dp0..\assets\shaders\shader.frag -o %~dp0..\assets\shaders\frag.spv
 
 EXIT /B %ERRORLEVEL%
