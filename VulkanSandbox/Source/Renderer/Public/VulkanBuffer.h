83c58a7b30da657172d2a0b7e9cbce3bad78f759
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-10-16 16:57:17 +0200

    Forgot to add the VulkanBuffer header :D

---
+++ VulkanSandbox/Source/Renderer/Public/VulkanBuffer.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "vulkan/vulkan_core.h"
+
+// Wrapper around Vulkan buffers backed by device memory
+struct VulkanBuffer
+{
+    VkDevice device_;
+    VkDeviceMemory memory_handle_ = VK_NULL_HANDLE;
+    VkBuffer buffer_handle_ = VK_NULL_HANDLE;
+    uint32_t size = 0;
+    
+    void Destroy();
+    
+    void* Map(VkDeviceSize size = VK_WHOLE_SIZE, VkDeviceSize offset = 0);
+    void Unmap();
+};
