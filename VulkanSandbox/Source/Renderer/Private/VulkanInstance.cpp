c3d2aec5bf761b25d6844cd620eee0caf61100b9
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-10-16 14:12:15 +0200

    Add instance abstraction

---
+++ VulkanSandbox/Source/Renderer/Private/VulkanInstance.cpp
@@ -0,0 +1,133 @@
+#include "VulkanInstance.h"
+
+void VulkanInstance::Init()
+{
+    VkInstanceCreateInfo create_info{};
+    create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+    // This is optional, but may provide crucial information to the graphics driver to optimize the application.
+    // E.g. we could provide information about our well-known engine (Unity, Unreal,...) which the driver knows about.
+    VkApplicationInfo app_info{};
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pApplicationName = "Vulkan Sandbox";
+    app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.pEngineName = "No Engine";
+    app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.apiVersion = VK_API_VERSION_1_0;
+    create_info.pApplicationInfo = &app_info;
+
+    QuerySupportedInstanceExtensions();
+    QuerySupportedInstanceValidationLayers();
+
+    VkDebugUtilsMessengerCreateInfoEXT debug_messenger_create_info;
+    SetupEnabledValidationLayers(create_info, debug_messenger_create_info);
+    SetupEnabledExtensions(create_info);
+
+    if (vkCreateInstance(&create_info, nullptr, &instance_) != VK_SUCCESS)
+    {
+        LOG_ERROR("Failed to create Vulkan instance.");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void VulkanInstance::Shutdown()
+{
+    CHECK(instance_ != VK_NULL_HANDLE);
+    vkDestroyInstance(instance_, nullptr);
+}
+
+void VulkanInstance::QuerySupportedInstanceExtensions()
+{
+    uint32 extension_count = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+    supported_extensions_.resize(extension_count);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, supported_extensions_.data());
+}
+
+void VulkanInstance::QuerySupportedInstanceValidationLayers()
+{
+    uint32_t layer_count;
+    vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+    supported_validation_layers_.resize(layer_count);
+    vkEnumerateInstanceLayerProperties(&layer_count, supported_validation_layers_.data());
+}
+
+void VulkanInstance::SetupEnabledValidationLayers(VkInstanceCreateInfo& in_create_info, VkDebugUtilsMessengerCreateInfoEXT& in_debug_messenger_create_info)
+{
+#ifdef _RENDER_DEBUG
+    required_validation_layers_.push_back("VK_LAYER_KHRONOS_validation");
+    in_create_info.enabledLayerCount = static_cast<uint32_t>(required_validation_layers_.size());
+    in_create_info.ppEnabledLayerNames = required_validation_layers_.data();
+
+    // Create an additional debug messenger which will automatically be used during vkCreateInstance and vkDestroyInstance and cleaned up after that.
+    in_debug_messenger_create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    in_debug_messenger_create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    in_debug_messenger_create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    in_debug_messenger_create_info.pfnUserCallback = VulkanDebugUtils::DebugCallback;
+    in_debug_messenger_create_info.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&in_debug_messenger_create_info;
+#else
+    in_create_info.enabledLayerCount = 0;
+    in_create_info.pNext = nullptr;
+#endif
+}
+
+void VulkanInstance::SetupEnabledExtensions(VkInstanceCreateInfo& in_create_info)
+{
+#ifdef _RENDER_DEBUG
+    required_extensions_.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+#endif
+
+    if (AreExtensionsSupport())
+    {
+        LOG("Enabled extenstions: ");
+        for(const char* ext : required_extensions_)
+        {
+            LOG("- {}", ext);
+        }
+
+        in_create_info.enabledExtensionCount = static_cast<uint32_t>(required_extensions_.size());
+        in_create_info.ppEnabledExtensionNames = required_extensions_.data();
+    }
+    else
+    {
+        LOG_ERROR("Failed to create Vulkan instance: Some required extensions are not supported.");
+        exit(EXIT_FAILURE);
+    }
+}
+
+bool VulkanInstance::AreExtensionsSupport()
+{
+    std::vector<const char*> missing_extensions;
+
+    for (const char* ext : required_extensions_)
+    {
+        bool found_extension = false;
+        for (const auto& supported_ext_property : supported_extensions_)
+        {
+            found_extension = strcmp(ext, supported_ext_property.extensionName) == 0;
+            if (found_extension)
+            {
+                break;
+            }
+        }
+
+        if(found_extension == false)
+        {
+            missing_extensions.push_back(ext);
+        }
+    }
+
+    bool all_exts_supported = missing_extensions.size() <= 0;
+    if(all_exts_supported == false)
+    {
+        LOG_ERROR("The following extensions are required but not supported: ");
+        for(const auto& ext : missing_extensions)
+        {
+            LOG_ERROR("- {}", ext);
+        }
+    }
+
+    return all_exts_supported;
+}
---
+++ VulkanSandbox/Source/Renderer/Public/VulkanInstance.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "VulkanDebugUtils.h"
+
+/**
+*   The instance is the connection between the application and the Vulkan library.
+*   We also tell the driver some more information, e.g. what validation layers or extensions we need.
+*/
+class VulkanInstance
+{
+public:
+    void Init();
+    void Shutdown();
+
+    VkInstance GetHandle() const
+    {
+        return instance_;
+    }
+
+private:
+    /**
+    *    Fills supported_instance_extensions_ with data queried from the driver.
+    */
+    void QuerySupportedInstanceExtensions();
+
+    /**
+    *    Fills supported_validation_layers_ with data queried from the driver.
+    */
+    void QuerySupportedInstanceValidationLayers();
+
+    /**
+    *    Tell the driver which global validation layers to enable and sets up the debug callback.
+    *    Automatically adds VK_LAYER_KHRONOS_validation to required layers if in render debug configuration.
+    *    
+    *    @param in_create_info The VkInstanceCreateInfo struct to fill.
+    *    @param in_debug_messenger_create_info  The VkDebugUtilsMessengerCreateInfoEXT struct to fill.
+    */
+    void SetupEnabledValidationLayers(VkInstanceCreateInfo& in_create_info, VkDebugUtilsMessengerCreateInfoEXT& in_debug_messenger_create_info);
+
+    /**
+    *   Tell the driver which global extensions to enable.
+    *   (Global extensions are extensions which are applied to the entire program instead of a specific device)
+    *   Automatically adds VK_EXT_DEBUG_UTILS_EXTENSION_NAME to required extensions if in render debug configuration.
+    *   
+    *   @param in_create_info The VkInstanceCreateInfo struct to fill.
+    */
+    void SetupEnabledExtensions(VkInstanceCreateInfo& in_create_info);
+
+    /**
+    *   Check if every required extension is actually available
+    *   
+    *   @return True if all required_extensions_ are supported.
+    */
+    bool AreExtensionsSupport();
+
+    VkInstance instance_ = VK_NULL_HANDLE;
+    std::vector<VkExtensionProperties> supported_extensions_;
+    std::vector<VkLayerProperties> supported_validation_layers_;
+
+    std::vector<const char*> required_extensions_ = { "VK_KHR_surface", "VK_KHR_win32_surface" }; // TODO: this should depend on the platform
+    std::vector<const char*> required_validation_layers_;
+};
