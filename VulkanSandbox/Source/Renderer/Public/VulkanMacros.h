17a16474f26f819145fda383bf933ac44e8c7e49
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-10-16 18:08:12 +0200

    Fix VERIFY_VK_RESULT

--- VulkanSandbox/Source/Renderer/Public/VulkanMacros.h
+++ VulkanSandbox/Source/Renderer/Public/VulkanMacros.h
@@ -1,7 +1,3 @@
 #pragma once
 
-#ifndef NDEBUG
-    #define VERIFY_VK_RESULT(VkCall) { const VkResult r = VkCall; CHECK_MSG(r != VK_SUCCESS, "Vulkan Call Failed!"); }
-#else
-    #define VERIFY_VK_RESULT(VkCall) { VkCall; }
-#endif
+#define VERIFY_VK_RESULT(VkCall) { VERIFY_MSG(VkCall == VK_SUCCESS, "Vulkan Call Failed!"); }
