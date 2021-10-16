cdd12217819953456bb0a91ff71184464e133cdb
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-10-16 13:09:25 +0200

    Add logging utilities

--- VulkanSandbox/Source/Core/Private/Log.cpp
+++ VulkanSandbox/Source/Core/Private/Log.cpp
@@ -1,13 +1,23 @@
 #include "Log.h"
 
-#include <stdio.h>
-#include <stdarg.h>
+#pragma warning(push, 0)
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#pragma warning(pop)
 
-void Log::PrintLine(const char* format_text, ...)
+SharedPtr<spdlog::logger> Log::logger_;
+
+void Log::Init()
 {
-    va_list args;
-    va_start(args, format_text);
-    vprintf(format_text, args);
-    va_end(args);
-    printf("\n");
+    std::vector<spdlog::sink_ptr> log_sinks;
+    log_sinks.emplace_back(MakeShared<spdlog::sinks::stdout_color_sink_mt>());
+    log_sinks[0]->set_pattern("%^[%T] %n: %v%$");
+
+    log_sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Saved/Logs/Log.txt", true));
+    log_sinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+    logger_ = MakeShared<spdlog::logger>("VK_SBX", begin(log_sinks), end(log_sinks));
+    spdlog::register_logger(logger_);
+    logger_->set_level(spdlog::level::trace);
+    logger_->flush_on(spdlog::level::trace);
 }
--- VulkanSandbox/Source/Core/Public/Log.h
+++ VulkanSandbox/Source/Core/Public/Log.h
@@ -1,6 +1,26 @@
 #pragma once
 
-namespace Log
+// This ignores all warnings raised inside External headers
+#pragma warning(push, 0)
+#include "spdlog/spdlog.h"
+#include "spdlog/fmt/ostr.h"
+#pragma warning(pop)
+
+class Log
 {
-    void PrintLine(const char* format_text, ...);
-}
+public:
+    static void Init();
+    static SharedPtr<spdlog::logger>& GetLogger()
+    {
+        return logger_;
+    }
+
+private:
+    static SharedPtr<spdlog::logger> logger_;
+};
+
+#define LOG(...)                Log::GetLogger()->info(__VA_ARGS__)
+#define LOG_TRACE(...)          Log::GetLogger()->trace(__VA_ARGS__)
+#define LOG_WARN(...)           Log::GetLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...)          Log::GetLogger()->error(__VA_ARGS__)
+#define LOG_CRITICAL(...)       Log::GetLogger()->critical(__VA_ARGS__)
