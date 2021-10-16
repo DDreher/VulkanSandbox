b665dc76128b44a853a009f50a45d3d66f56b713
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-10-16 22:08:36 +0200

    Add VulkanUtils

---
+++ VulkanSandbox/VulkanUtils.h
@@ -0,0 +1,32 @@
+#pragma once
+
+struct VulkanUtils
+{
+    static bool IsInList(const char* s, const std::vector<const char*> l)
+    {
+        for(const auto entry : l)
+        {
+            bool found = strcmp(entry, s) == 0;
+            if (found)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static bool IsListSubset(const std::vector<const char*> l, const std::vector<const char*> subset_list)
+    {
+        for (const char* entry : subset_list)
+        {
+            bool found = IsInList(entry, l);
+            if(found == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
