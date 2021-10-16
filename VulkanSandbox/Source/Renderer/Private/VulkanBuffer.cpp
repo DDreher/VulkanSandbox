11f4ee8a565e5ef04d3bdefcbb81ce511a46cccf
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-10-16 18:06:38 +0200

    And the renamed cpp.. God dammit!

---
+++ VulkanSandbox/Source/Renderer/Private/VulkanBuffer.cpp
@@ -0,0 +1,33 @@
+#include "VulkanBuffer.h"
+
+#include "assert.h"
+
+void VulkanBuffer::Destroy()
+{
+    assert(device_ != VK_NULL_HANDLE);
+    assert(buffer_handle_!= VK_NULL_HANDLE);
+    assert(memory_handle_ != VK_NULL_HANDLE);
+
+    vkDestroyBuffer(device_, buffer_handle_, nullptr);
+    vkFreeMemory(device_, memory_handle_, nullptr);
+}
+
+void* VulkanBuffer::Map(VkDeviceSize size, VkDeviceSize offset)
+{
+    assert(device_ != VK_NULL_HANDLE);
+    assert(memory_handle_ != VK_NULL_HANDLE);
+    assert(size != 0);
+    static const VkMemoryMapFlags FLAGS = 0;
+
+    void* mapped_addr = nullptr;
+    assert(vkMapMemory(device_, memory_handle_, offset, size, FLAGS, &mapped_addr) == VK_SUCCESS);
+
+    return mapped_addr;
+}
+
+void VulkanBuffer::Unmap()
+{
+    assert(device_ != VK_NULL_HANDLE);
+    assert(memory_handle_ != VK_NULL_HANDLE);
+    vkUnmapMemory(device_, memory_handle_);
+}
