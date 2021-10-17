b848c42b37525d03d19b1853e2a50a952ce8fc51
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-10-17 10:27:15 +0200

    Set validation message severity to warning

--- VulkanSandbox/Source/Renderer/Public/VulkanDebugUtils.h
+++ VulkanSandbox/Source/Renderer/Public/VulkanDebugUtils.h
@@ -6,7 +6,7 @@ struct VulkanDebugUtils
     static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity, VkDebugUtilsMessageTypeFlagsEXT message_type,
         const VkDebugUtilsMessengerCallbackDataEXT* callback_data, void* user_data)
     {
-        LOG_ERROR("VK Validation Layer: {}", callback_data->pMessage);
+        LOG_WARN("VK Validation Layer: {}", callback_data->pMessage);
 
         // Return value indicates if the Vulkan call that triggered the validation layer message should be aborted with VK_ERROR_VALIDATION_FAILED_EXT.
         // Usually this is only used to test validation layers. -> We should most likely always return VK_FALSE here.
