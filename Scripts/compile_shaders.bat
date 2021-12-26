bf888fcc95006574b2fb157294f72932f411c913
Daniel Dreher <4201607+DDreher@users.noreply.github.com>
2021-12-26 22:43:20 +0100

    This should actually not be the 32 bit version.

--- Scripts/compile_shaders.bat
+++ Scripts/compile_shaders.bat
@@ -1,4 +1,4 @@
-%VULKAN_SDK%\Bin32\glslc.exe %~dp0..\assets\shaders\shader.vert -o %~dp0..\assets\shaders\vert.spv
-%VULKAN_SDK%\Bin32\glslc.exe %~dp0..\assets\shaders\shader.frag -o %~dp0..\assets\shaders\frag.spv
+%VULKAN_SDK%\Bin\glslc.exe %~dp0..\assets\shaders\shader.vert -o %~dp0..\assets\shaders\vert.spv
+%VULKAN_SDK%\Bin\glslc.exe %~dp0..\assets\shaders\shader.frag -o %~dp0..\assets\shaders\frag.spv
 
 EXIT /B %ERRORLEVEL%
